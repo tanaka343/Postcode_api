9d4241d499dd8e56d9db71f26f06a8d540b9e071
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-06 15:58:50 +0900

    feat : 郵便番号からデータ取得するエンドポイント作成

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI
+from typing import Optional
+from schemas import ItemResponse
 
 app = FastAPI()
 
 @app.get("/")
 def top():
   return "Postcode_api"
+
+class Postcode:
+  def __init__(
+      self,
+      address1 : str,
+			address2: str,
+			address3: Optional[str],
+			kana1: str,
+			kana2: str,
+			kana3: Optional[str],
+			prefcode: str,
+			zipcode: str
+      ):
+    self.address1=address1
+    self.address2=address2
+    self.address3=address3
+    self.kana1=kana1
+    self.kana2=kana2
+    self.kana3=kana3
+    self.prefcode=prefcode
+    self.zipcode=zipcode
+
+Items = [Postcode("北海道","札幌市中央区","","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","","01101","0640941"),
+         Postcode("北海道","札幌市中央区","旭ケ丘","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","01101","0640941"),
+         Postcode("北海道","札幌市中央区","大通東","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","01101","0600041")]   
+ 
+# row_Postcode = [[01101,"060  ","0600000","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","","北海道","札幌市中央区","",0,0,0,0,0,0],
+# [01101,"064  ","0640941","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0],
+# [01101,"060  ","0600041","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","北海道","札幌市中央区","大通東",0,0,1,0,0,0]
+# ]
+
+@app.get("/api/search",response_model=ItemResponse)
+def find_by_zipcode(zipcode :str):
+  for item in Items:
+    if item.zipcode == zipcode:
+      return item
