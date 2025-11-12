2b8173c79888beef7eb674e6b28f12be561ecd43
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-12 15:43:59 +0900

    feat : get_db関数の追加

--- database.py
+++ database.py
@@ -7,3 +7,10 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
