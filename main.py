74b9faaf9ddfe4b4c9acc11fa5e6df9637e2180d
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-13 15:03:10 +0900

    feat : エンドポイントをデータベースから情報を取得する形に変更
    -DbDependencyの定義、型ヒントでの使用
    -仮データのコメントアウト
    -エンドポイントをデータベースから取得
    -limitのif文を==から=へ修正

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-from fastapi import FastAPI,HTTPException,Request,status
+from fastapi import FastAPI,HTTPException,Request,status,Depends
 from fastapi.responses import JSONResponse
-from typing import Optional,List
+from typing import Optional,List,Annotated
 from schemas import ItemResponse,Address
 from fastapi.exceptions import RequestValidationError
-
+from database import get_db
+from sqlalchemy.orm import Session
+from models import Item
+DbDependency = Annotated[Session,Depends(get_db)]
 app = FastAPI()
 
 @app.get("/")
 def top():
   return "Postcode_api"
 
-class Postcode:
-  def __init__(
-      self,
-      address1 : str,
-			address2: str,
-			address3: Optional[str],
-			kana1: str,
-			kana2: str,
-			kana3: Optional[str],
-			prefcode: str,
-			zipcode: str
-      ):
-    self.address1=address1
-    self.address2=address2
-    self.address3=address3
-    self.kana1=kana1
-    self.kana2=kana2
-    self.kana3=kana3
-    self.prefcode=prefcode
-    self.zipcode=zipcode
+#仮データ
+# class Postcode:
+#   def __init__(
+#       self,
+#       address1 : str,
+# 			address2: str,
+# 			address3: Optional[str],
+# 			kana1: str,
+# 			kana2: str,
+# 			kana3: Optional[str],
+# 			prefcode: str,
+# 			zipcode: str
+#       ):
+#     self.address1=address1
+#     self.address2=address2
+#     self.address3=address3
+#     self.kana1=kana1
+#     self.kana2=kana2
+#     self.kana3=kana3
+#     self.prefcode=prefcode
+#     self.zipcode=zipcode
 
-Items = [Postcode("北海道","札幌市中央区","","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","","01101","0640941"),
-         Postcode("北海道","札幌市中央区","旭ケ丘","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","01101","0640941"),
-         Postcode("北海道","札幌市中央区","大通東","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","01101","0600041")]   
+# Items = [Postcode("北海道","札幌市中央区","","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","","01101","0640941"),
+#          Postcode("北海道","札幌市中央区","旭ケ丘","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","01101","0640941"),
+#          Postcode("北海道","札幌市中央区","大通東","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","01101","0600041")]   
  
-# row_Postcode = [[01101,"060  ","0600000","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","","北海道","札幌市中央区","",0,0,0,0,0,0],
-# [01101,"064  ","0640941","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0],
-# [01101,"060  ","0600041","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","北海道","札幌市中央区","大通東",0,0,1,0,0,0]
-# ]
 
 @app.exception_handler(RequestValidationError)
 async def validation_handler(request: Request,exc: RequestValidationError):
@@ -64,12 +64,12 @@ async def HTTPException_handler(request: Request,exc: HTTPException):
 
 
 @app.get("/api/search",response_model=ItemResponse[List[Address]],status_code=status.HTTP_200_OK)
-def find_by_zipcode(zipcode :str,limit :Optional[int]=None):
+def find_by_zipcode(db : DbDependency,zipcode :str,limit :Optional[int]=None):
 
   if limit is None:
-    limit == 20
+    limit = 20
   else:
-    limit == limit
+    limit = limit
     
   if len(str(zipcode))!=7:
     raise HTTPException(status_code=400,
@@ -80,14 +80,10 @@ def find_by_zipcode(zipcode :str,limit :Optional[int]=None):
     raise HTTPException(status_code=400,
                         detail="パラメータ「郵便番号」に数字以外の文字が指定されています。")
   try:
-    item_list = []
-    for item in Items:
-      if item.zipcode == zipcode:
-        item_list.append(item)
-    limited_list = item_list[:limit]  
+    found_items = db.query(Item).filter(Item.zipcode == zipcode).order_by(Item.zipcode).limit(limit).all()
     return {
       "message" : "",
-      "result" : limited_list,
+      "result" : found_items,
       "status" : 200
     }
   except Exception as e:
