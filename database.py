544f8e87c7200e9ea391741fbc9a65274cb27880
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-15 11:17:08 +0900

    refactor : データベースのURLをMySQLに変更

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+# DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = "mysql+pymysql://appuser:apppass@localhost:3306/postcode_db"
 
-DATABASE_URL = "sqlite:///./database.db"
+# DATABASE_URL = os.getenv(
+#   "DATABASE_URL",
+#   "sqlite:///./database.db"
+# )
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
